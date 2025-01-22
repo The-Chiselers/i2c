25f5ee2a0d269841d6f2c050cf1d0162569841e5
algebru <ahmedmensh78@gmail.com>
2025-01-22 02:12:48 +0000

    master addressing rtl

--- src/main/scala/Interfaces.scala
+++ src/main/scala/Interfaces.scala
@@ -20,14 +20,14 @@ class ApbInterface(p: BaseParams) extends Bundle {
 
 class MasterInterface() extends Bundle {
   val scl    = Output(Bool())  // I2C clock output (master)
-  val sdaIn     = Input(Bool())
-  val sdaOut    = Output(Bool())  // I2C data output (master)
+  val sdaIn     = Input(UInt(1.W))
+  val sdaOut    = Output(UInt(1.W))  // I2C data output (master)
 }
 
 class SlaveInterface() extends Bundle {
   val scl     = Input(Bool())   // I2C clock input (slave mode)
-  val sdaIn     = Input(Bool())   // I2C data input (slave mode)'
-  val sdaOut    = Output(Bool())
+  val sdaIn     = Input(UInt(1.W))   // I2C data input (slave mode)'
+  val sdaOut    = Output(UInt(1.W))
 }
 
 class I2CInterface(p: BaseParams) extends Bundle {
